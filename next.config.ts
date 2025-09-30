552b150df82ac1ba0d384967e123248128de5509
Angel Espinoza <angelezpin@MacBook-Air-de-Angel.local>
2025-09-30 16:39:24 -0600

    Configurar Next para ignorar errores de build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // ❌ No detengas el build aunque haya errores de ESLint
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // ❌ No detengas el build aunque haya errores de TypeScript
+    ignoreBuildErrors: true,
+  },
+  reactStrictMode: true,
 };
 
 export default nextConfig;
