b146a3ca13d392542fe4c3b9cfab5326bf843a70
aezpinozarr <aezpinozarr@gmail.com>
2025-10-01 09:14:19 -0600

    Update page.tsx

--- app/sesiones/calendario/page.tsx
+++ app/sesiones/calendario/page.tsx
@@ -30,6 +30,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 
 const API_BASE = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000";
+console.log("🌍 API_BASE:", API_BASE);
 
 // ======================
 // Tipos
