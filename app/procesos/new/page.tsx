5271b72e2926474e8fc5dd16eccc6016a99aba7b
Angel Espinoza <angelezpin@MacBook-Air-de-Angel.local>
2025-11-28 16:07:20 -0600

    cambio

--- app/procesos/new/page.tsx
+++ app/procesos/new/page.tsx
@@ -1,4 +1,6 @@
 "use client";
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
 
 import { useRouter, useSearchParams } from "next/navigation";
 import Link from "next/link";
